f340a88bf4b8ed678d62862e3fb745418f8ca881
LettyC23 <44488698+LettyC23@users.noreply.github.com>
2020-12-09 15:01:37 -0600

    Api_Rest_Android

---
+++ API_REST_Android/altas_proveedores.php
@@ -0,0 +1,45 @@
+<?php
+
+include('../Conexion_BD/conexion_bd.php');
+
+    $con = new ConexionBD();
+    $conexion = $con->getConexion();
+echo"-----------------------------------------------------------------------------------------------------------";
+    var_dump($conexion);
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        $cadena_JSON = file_get_contents('php://input'); // prepara PHP para recibir informacion a traves de HTTP
+
+        if($cadena_JSON == false){
+            echo "No hay peticion HTTP";
+        }else{
+            $datos = json_decode($cadena_JSON, TRUE);
+
+            $nc = $datos['nc'];
+            $np= $datos['np'];
+            $ne = $datos['ne'];
+            $d = $datos['d'];
+            $t = $datos['t'];
+    
+            $sql = "INSERT INTO Proveedores VALUES('$nc','$np','$ne','$d','$t')";
+            
+            $res = mysqli_query($conexion, $sql);
+            
+            $respuesta = array();
+            if($res){
+                //todo bien
+                $respuesta['exito'] = true;
+                $respuesta['mensaje'] = "Inserccion correcta";
+                echo json_encode($respuesta);
+                //var_dump($cad);
+            }else{
+                //todo mal
+                $respuesta['exito'] = false;
+                $respuesta[] = "Insercion incorrecta";
+                echo json_encode($respuesta);
+            }
+        }
+        
+    }
+?>
--- Modelos/procesar_validacion_usuarios.php
+++ Modelos/procesar_validacion_usuarios.php
@@ -13,13 +13,14 @@ include('../Conexion_BD/conexion_bd_usuarios.php');
         $contraseña_cifrado = SHA1($contraseña);
 
     $sql = "SELECT * FROM Usuarios WHERE NombreDeUsuario='$usuario_cifrado' AND Contrasenia='$contraseña_cifrado'";
-
+   
     $res = mysqli_query($conexion, $sql);
 
     if(mysqli_num_rows($res)>0){
         session_start();
         $_SESSION['autenticado'] = true;
-        $_SESSION['usuario'] = $usuario;
+        $_SESSION['usuario'] = $usuario_cifrado;
+        $_SESSION['contraseña'] = $contraseña_cifrado;
         
         header('location:../Vista/formulario_dashboard.php');
         
